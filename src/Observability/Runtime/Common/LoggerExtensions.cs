3bb768fa9daa7493806dd47c67e41bc54d54213b
Julio Menendez <juliome@microsoft.com>
2025-10-31 09:07:10 -0700

    Adds empty methods for logging.

---
+++ src/Observability/Runtime/Common/LoggerExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Agents.A365.Observability.Runtime.Tracing.Contracts;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+
+
+namespace Microsoft.Agents.A365.Observability.Runtime.Common
+{
+    /// <summary>
+    /// Provides helper methods to log our scopes
+    /// </summary>
+    public static partial class LoggerExtensions
+    {
+        /// <summary>
+        /// Logs an invoke_agent event.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="invokeAgentDetails"></param>
+        /// <param name="tenantDetails"></param>
+        public static void LogInvokeAgent(
+            this ILogger logger, 
+            [LogProperties(OmitReferenceName = true)] in InvokeAgentDetails invokeAgentDetails,
+            [LogProperties(OmitReferenceName = true)] in TenantDetails tenantDetails)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Logs an inference call event.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="inferenceCallDetails"></param>
+        /// <param name="agentDetails"></param>
+        /// <param name="tenantDetails"></param>
+        public static void LogInferenceCall(
+            this ILogger logger,
+            [LogProperties(OmitReferenceName = true)] in InferenceCallDetails inferenceCallDetails,
+            [LogProperties(OmitReferenceName = true)] in AgentDetails agentDetails,
+            [LogProperties(OmitReferenceName = true)] in TenantDetails tenantDetails)
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Logs a tool call event.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="toolCallDetails"></param>
+        /// <param name="agentDetails"></param>
+        /// <param name="tenantDetails"></param>
+        public static void LogToolCall(
+            this ILogger logger,
+            [LogProperties(OmitReferenceName = true)] in ToolCallDetails toolCallDetails,
+            [LogProperties(OmitReferenceName = true)] in AgentDetails agentDetails,
+            [LogProperties(OmitReferenceName = true)] in TenantDetails tenantDetails)
+        {
+            // TODO
+        }
+    }
+}
