11b7c86473b0bb9ee1f99f24e065babff44eab3f
Julio Menendez <juliome@microsoft.com>
2025-11-07 16:41:44 -0700

    Prepares to send logs to ETW.

---
+++ src/Observability/Hosting/Etw/EtwLogProcessor.cs
@@ -0,0 +1,19 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Logs;
+
+namespace Microsoft.Agents.A365.Observability.Hosting.Etw
+{
+    /// <summary>
+    /// Processes logs by emitting ETW events.
+    /// </summary>
+    public class EtwLogProcessor : BaseProcessor<LogRecord>
+    {
+        /// <summary>
+        /// Emits an ETW event with log details.
+        /// </summary>
+        public override void OnEnd(LogRecord data)
+        {
+            // TODO: Implement log formatting and emit ETW using LogJson.
+        }
+    }
+}
--- src/Observability/Hosting/Etw/EtwServiceCollectionExtensions.cs
+++ src/Observability/Hosting/Etw/EtwServiceCollectionExtensions.cs
@@ -15,12 +15,22 @@ namespace Microsoft.Agents.A365.Observability.Hosting.Etw
         /// <summary>
         /// Adds OpenTelemetry tracing with ETW to the service collection.
         /// </summary>
-        public static IServiceCollection AddTracingWithEtw(this IServiceCollection services, Action<EtwBuilder>? configure = null)
+        public static IServiceCollection AddTracingWithEtw(this IServiceCollection services, Action<EtwTracingBuilder>? configure = null)
         {
-            var builder = new EtwBuilder(services);
+            var builder = new EtwTracingBuilder(services);
 
             configure?.Invoke(builder);
             
+            return builder.Build();
+        }
+
+        /// <summary>
+        /// Adds OpenTelemetry logging with ETW to the service collection.
+        /// </summary>
+        public static IServiceCollection AddLoggingWithEtw(this IServiceCollection services, Action<EtwLoggingBuilder>? configure = null)
+        {
+            var builder = new EtwLoggingBuilder(services);
+            configure?.Invoke(builder);
             return builder.Build();
         }
     }
