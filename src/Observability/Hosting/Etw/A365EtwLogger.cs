f6bb7d29e0ddfee8697e465c8db9115338a58fee
Julio Menendez <juliome@microsoft.com>
2025-11-10 16:10:30 -0700

    Adds missing copyright headers.

--- src/Observability/Hosting/Etw/A365EtwLogger.cs
+++ src/Observability/Hosting/Etw/A365EtwLogger.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Agents.A365.Observability.Runtime.DTOs.Builders;
+﻿// ------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------------
+
+using Microsoft.Agents.A365.Observability.Runtime.DTOs.Builders;
 using Microsoft.Agents.A365.Observability.Runtime.Tracing.Contracts;
 using Microsoft.Extensions.Logging;
 using System;
--- src/Observability/Hosting/Etw/Constants.cs
+++ src/Observability/Hosting/Etw/Constants.cs
@@ -1,4 +1,8 @@
-﻿namespace Microsoft.Agents.A365.Observability.Hosting.Etw
+﻿// ------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------------
+
+namespace Microsoft.Agents.A365.Observability.Hosting.Etw
 {
     /// <summary>
     /// Constants used in ETW logging.
